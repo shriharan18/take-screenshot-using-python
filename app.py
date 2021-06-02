4172ea57de5e27e792d645a164f639c6d9e9e20a
Shriharan <74127448+tonyshree18@users.noreply.github.com>
2021-06-02 15:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-import pyautogui
+# This code was written by Shreharan for the Youtube Channel Stark Intelligence
+import pyautogui        # importing module
 import tkinter as tk
 from tkinter import filedialog
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, width=300, height=300)
+canvas = tk.Canvas(root, width=300, height=300)     # creating a window
 canvas.pack()
 
 
-def takeScreenshot():
+def takeScreenshot():       # creating a function
     myScreenshot = pyautogui.screenshot()
     file_path = filedialog.asksaveasfilename(defaultextension='.png')
     myScreenshot.save(file_path)
 
 
-myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='yellow', fg='black', font=10)
+myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='yellow', fg='black', font=10)      # creating a button
 canvas.create_window(150, 150, window=myButton)
 
 root.mainloop()
